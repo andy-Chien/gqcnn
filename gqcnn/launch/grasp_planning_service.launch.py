0975a597110550fd85a84c113af76d1d460eac6a
Andy Chien <808790017@gms.tku.edu.tw>
2023-06-29 11:59:07 +0800

    run pj and suction at the same time

--- gqcnn/launch/grasp_planning_service.launch.py
+++ gqcnn/launch/grasp_planning_service.launch.py
@@ -12,27 +12,36 @@ def generate_launch_description():
   gqcnn_ros_share_dir = get_package_share_directory('gqcnn')
 
   ros_param_file = LaunchConfiguration('ros_param_file', default = gqcnn_ros_share_dir + '/config/ros.yaml')
-  # print(gqcnn_ros_share_dir)
   
   declare_ros_param_file_cmd = DeclareLaunchArgument(
       'ros_param_file',
       default_value = gqcnn_ros_share_dir + '/config/ros.yaml',
       description = 'Path to file with ROS related config')  
-
-
-
-  grasp_planning_service_cmd = Node(
-    namespace = 'gqcnn',
+  
+  pj_grasp_service = Node(
+    namespace = 'gqcnn_pj',
     package='gqcnn',
     executable='grasp_planner_node.py',
     name='grasp_planner',
     output='screen',
     parameters=[ros_param_file,
-    ])
+    ]
+  )
+  
+  suc_grasp_service = Node(
+    namespace = 'gqcnn_suc',
+    package='gqcnn',
+    executable='grasp_planner_node.py',
+    name='grasp_planner',
+    output='screen',
+    parameters=[ros_param_file,
+    ]
+  )
 
   ld = LaunchDescription()
 
   ld.add_action(declare_ros_param_file_cmd)  
-  ld.add_action(grasp_planning_service_cmd)
+  ld.add_action(pj_grasp_service)
+  ld.add_action(suc_grasp_service)
 
   return ld
